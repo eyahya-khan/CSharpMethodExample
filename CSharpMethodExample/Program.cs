06fb91de773c388120677bda64783a233a4eff86
eyahya-khan <eyahyakhan@gmail.com>
2021-06-13 12:06:32 +0200

    Add project files.

---
+++ CSharpMethodExample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpMethodExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
